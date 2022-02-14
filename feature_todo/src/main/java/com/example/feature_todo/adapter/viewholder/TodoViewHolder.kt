26b17b12b799ed12f70ca14a37345435a3de41d8
Rynn Arriola <rynn.arriola@yahoo.com>
2022-02-14 11:24:52 -0500

    fix the bugged

--- feature_todo/src/main/java/com/example/feature_todo/adapter/viewholder/TodoViewHolder.kt
+++ feature_todo/src/main/java/com/example/feature_todo/adapter/viewholder/TodoViewHolder.kt
@@ -28,7 +28,7 @@ class TodoViewHolder(
             when (event?.action) {
                 MotionEvent.ACTION_DOWN -> itemView.setBackgroundColor(Color.YELLOW)
                 MotionEvent.ACTION_UP -> itemView.setBackgroundColor(Color.WHITE)
-                MotionEvent.ACTION_UP -> itemView.setBackgroundColor(Color.WHITE)
+                MotionEvent.ACTION_CANCEL -> itemView.setBackgroundColor(Color.WHITE)
             }
             v?.onTouchEvent(event) ?: true
         }
